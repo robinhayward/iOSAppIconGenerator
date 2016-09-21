b4956a7aa0c700b03ff52975e94765b975b070a1
Gabrielle Iskandar <gabiskandar@gmail.com>
2016-09-21 10:09:30 +0300

    Re-ordered icon file names to match order requested by Apple.

--- IconGenerator.sh
+++ IconGenerator.sh
@@ -23,21 +23,17 @@ sips --resampleWidth 58   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iP
 sips --resampleWidth 87   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPhoneSpotlight29@3x.png"
 sips --resampleWidth 80   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPhoneSpotlight40@2x.png"
 sips --resampleWidth 120  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPhoneSpotlight40@3x.png"
-
-# App Icon
 sips --resampleWidth 120  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPhoneApp60@2x.png"
 sips --resampleWidth 180  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPhoneApp60@3x.png"
-sips --resampleWidth 76   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadApp76.png"
-sips --resampleWidth 152  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadApp76@2x.png"
-sips --resampleWidth 167  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadApp83.5@2x.png"
-
-# Spotlight Icon
-sips --resampleWidth 40   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSpotlight40.png"
-sips --resampleWidth 80   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSpotlight40@2x.png"
-
-# Settings Icon
+sips --resampleWidth 20   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadNotification20.png"
+sips --resampleWidth 40   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadNotification20@2x.png"
 sips --resampleWidth 29   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSettings29.png"
 sips --resampleWidth 58   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSettings29@2x.png"
+sips --resampleWidth 40   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSpotlight40.png"
+sips --resampleWidth 80   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadSpotlight40@2x.png"
+sips --resampleWidth 76   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadApp76.png"
+sips --resampleWidth 152  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadApp76@2x.png"
+sips --resampleWidth 167  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}iPadProApp83.5@2x.png"
 
 # Open the Output folder of created assets
 open ./Output/
