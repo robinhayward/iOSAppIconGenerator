a15d70818c1e5ac07c709865f848b93c50c0c1f1
Gabrielle Iskandar <gabiskandar@gmail.com>
2016-09-21 10:32:21 +0300

    Added WatchOS required icon sizes.

--- IconGenerator.sh
+++ IconGenerator.sh
@@ -47,5 +47,15 @@ sips --resampleWidth 512  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}Ma
 sips --resampleWidth 512  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}Mac512.png"
 sips --resampleWidth 1024 $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}Mac512@2x.png"
 
+# WatchOS
+sips --resampleWidth 48   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchNotification38mm@2x.png"
+sips --resampleWidth 55   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchNotification42mm@2x.png"
+sips --resampleWidth 58   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchCompanionSettings29@2x.png"
+sips --resampleWidth 87   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchCompanionSettings29@3x.png"
+sips --resampleWidth 40   $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchHomeScreen40.png"
+sips --resampleWidth 172  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchShortLook38mm@2x.png"
+sips --resampleWidth 196  $SOURCE_IMAGE_PATH --out "${SOURCE_OUTPUT_DIRECTORY}WatchShortLook42mm@2x.png"
+
+
 # Open the Output folder of created assets
 open ./Output/
